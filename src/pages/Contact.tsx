24863955137881eb106ed56789e970f481352528
jeanphilippe16 <135238141+jeanphilippe16@users.noreply.github.com>
2025-01-22 23:01:48 +0000

    sav

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -11,7 +11,7 @@ export const Contact = () => {
     { type: 'calls', icon: Phone, link: 'tel:+2250151205774', value: '+225 01 51 20 57 74' },
     { type: 'tiktok', icon: Phone, link: 'http://www.tiktok.com/@1966visualstiktok', value: '1966 Visualstiktok'},
     { type: 'instagram', icon: Instagram, link: 'https://www.instagram.com/1966visuals?igsh=MXA5Mmw2a3Y5Mjd6OQ==', value: '1966 Visuals'},
-    { type: 'youtube', icon: Youtube, link: 'https//www.youtube/@1966visuals-off', value: '1966visuals-off'}
+    { type: 'youtube', icon: Youtube, link: 'https://www.youtube/@1966visuals-off', value: '1966visuals-off'}
   ];
 
   return (
