71d4289f6b2789932099588e2eee636041766ebe
jeanphilippe16 <135238141+jeanphilippe16@users.noreply.github.com>
2025-01-23 08:34:55 +0000

    saV

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Mail, Facebook, Instagram, Youtube, MessageCircle } from 'lucide-react';
+import { Mail, Facebook, Instagram, Youtube, MessageCircle,Phone } from 'lucide-react';
 import { TikTokIcon } from '../components/TikTokIcon';
 
 export const Contact = () => {
